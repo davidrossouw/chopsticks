fc2284307471eb3906ea3f92d9a0aeb2b3ee90f2
davidrossouw <davidrossouw@gmail.com>
2020-01-12 18:12:26 -0500

    refactor: removed Game class and better leverage graph approach

--- model.py
+++ model.py
@@ -1,312 +1,171 @@
 import random
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 
+ALL_MOVES = [[0,0],[0,1],[1,0],[1,1]]
 
-class Hand():
-    def __init__(self, side='l'):
-        self.count = 1
-        self.side = {'l': 'left', 'r': 'right'}.get(side)
+class Move():
+    def __init__(self, player:int, move):
+        self.move = move
+        self.player = player # 0 or 1
+        self.non_player = 1 - player # 0 or 1
+        assert move in ALL_MOVES       
 
-
-class Player():
-    def __init__(self, name):
-        self.name = name
-        self.left = Hand('l')
-        self.right = Hand('r')
-
-    def get_state(self):
-        return (self.left.count, self.right.count)
-    
-    def reset(self):
-        self.left = Hand('l')
-        self.right = Hand('r')
+class Board():
+    def __init__(self, player1:tuple, player2:tuple):
+        self.state = [player1, player2]
         
+        
+    def moves_available(self, player:int=0) -> list: # make this have a Board as input
+        # [[0,0],[0,1],[1,0],[1,1]] # 'll', 'lr', 'rl', 'rr'
+        moves = []
+        active = player
+        inactive = 1-player
+        
+        # player1 left hand not 0 and player2 left hand not 0
+        if self.state[active][0] != 0 and self.state[inactive][0] != 0:
+            moves.append([0,0])
+            
+        # player1 left hand not 0 and player2 left hand not 0
+        if self.state[active][0] != 0 and self.state[inactive][1] != 0:
+            moves.append([0,1])
+            
+        # player1 right hand not 0 and player2 left hand not 0
+        if self.state[active][1] != 0 and self.state[inactive][0] != 0:
+            moves.append([1,0])
+            
+        # player1 right hand not 0 and player2 left hand not 0
+        if self.state[active][1] != 0 and self.state[inactive][1] != 0:
+            moves.append([1,1])
+
+        return moves
+        
+    def display(self):
+        return str(self.state[0][0])+str(self.state[0][1])+ \
+               str(self.state[1][0])+str(self.state[1][1])
+    
+    
+    def is_winner(self):
+        # player1 left hand == 0 and player1 right hand == 0
+        if self.state[0][0] == 0 and self.state[0][1] == 0:
+            return True
+            
+        # player2 left hand == 0 and player2 right hand == 0
+        if self.state[1][0] == 0 and self.state[1][1] == 0:
+            return True      
+        
+        return False
+
+
         
 class RandomPlayer():
-    def __init__(self, name):
+    def __init__(self, name, idx):
         self.name = name
-        self.left = Hand('l')
-        self.right = Hand('r')
+        self.idx = idx
+    
+    def select_move(self, board) -> Move:
+        moves_available = board.moves_available(self.idx)
+        move = random.choice(moves_available)
+        return Move(self.idx, move)
+    
+    
+def search(board:Board, max_depth=3):
+    """
+    Run game simulations from current game state to a maximum number
+    of moves ahead (max_depth)
+    Return the graph of possible moves and outcomes
+    state = [(1,1), (1,1)]
+    """
 
-    def get_state(self):
-        return (self.left.count, self.right.count)
+    n = 0 # node label which also serves as a node counter
+    depth = 0
     
-    def reset(self):
-        self.left = Hand('l')
-        self.right = Hand('r')
+    G = nx.DiGraph()
+    G.add_node(0, move=None, winner=None, player=depth%2, board=board.state, board_p = board.display())
     
-    @staticmethod
-    def move(game) -> str:
-        possible_moves = game.get_possible_moves()
-        move = random.choice(possible_moves)        
-        return move
+    # First branch in look ahead
+    newleavelist=[]
+    parent_node = n
+    parent_board = Board(G.nodes[n]['board'][0], G.nodes[n]['board'][1])
+    
+    for move in ALL_MOVES:
+        moves_available = parent_board.moves_available(player=depth)
+        if move not in moves_available:
+            continue
         
-
-class MinMaxPlayer():
-    def __init__(self, name, max_depth=5):
-        self.name = name
-        self.left = Hand('l')
-        self.right = Hand('r')
-        self.max_depth = max_depth
-
-    def get_state(self):
-        return (self.left.count, self.right.count)
-    
-    def reset(self):
-        self.left = Hand('l')
-        self.right = Hand('r')
+        # Do move
+        new_board = update_board(Move(player=depth%2, move=move), parent_board)
         
-    @staticmethod
-    def get_path(G, node_start, node_end, data=None, include_root=False):
-        for child in G.predecessors(node_end):
-            path = [G.nodes[node_end].get(data)]
-            while child != node_start:
-                path.insert(0, G.nodes[child].get(data))
-                child = next(G.predecessors(child))
-
-            if include_root:
-                path.insert(0, G.nodes[node_start].get(data))
-            return path
+        # Add move node to graph
+        n=n+1
+        G.add_node(n, move=move, winner=new_board.is_winner(), player=depth%2, board=new_board.state, board_p = new_board.display())
+        G.add_edge(parent_node, n)
+        if new_board.is_winner():
+            continue
+        newleavelist.append(n)
     
-    def eval_game_tree(self, game):
-        """
-        Run game simulations from current game state to a maximum number
-        of moves ahead (max_depth)
-        Return the graph of possible moves and outcomes, and the current (root) node
-        """
-
-        if self.max_depth < 1: return
-
-        G = game.graph.copy()
-        all_moves = ['ll','lr', 'rl', 'rr']
-        root = game.turn-1
-        n = root # node label which also serves as a node counter
-        newleavelist=[]
-        # First branch in look ahead
-        #game.switch_players()
-        for attack in all_moves:
-            n=n+1
-            # Add move node to graph
-            G.add_node(n, attack=attack, winner=None, player=None)
-            G.add_edge(root, n)
-            attack_sequence = self.get_path(G, 0, n, 'attack', include_root=False)
-            ## Gun game simulation:
-            minigame = Game(Player(game.active.name), Player(game.inactive.name))
-            is_legal = minigame.check_moves(attack_sequence)
-            if not is_legal:
-                G.remove_node(n)
-                continue
-            G.nodes[n]['player'] = minigame.inactive.name
-            if minigame.winner:
-                G.nodes[n]['winner'] = minigame.winner
-                continue
-
-            newleavelist.append(n)
-        depth=1
-        # subsequent branches
-        while depth < self.max_depth:
-            leavelist = newleavelist[:]
-            newleavelist = []
-            for leave in leavelist:
-                for attack in all_moves:
-                    n=n+1
-                    # Add move node to graph
-                    G.add_node(n, attack=attack, winner=None, player=None)
-                    G.add_edge(leave, n)
-                    attack_sequence = self.get_path(G, 0, n, 'attack', include_root=False)
-                    ## Gun game simulation:
-                    minigame = Game(Player(game.active.name), Player(game.inactive.name))
-                    is_legal = minigame.check_moves(attack_sequence)
-                    if not is_legal:
-                        G.remove_node(n)
-                        continue
-                    G.nodes[n]['player'] = minigame.inactive.name
-                    if minigame.winner:
-                        # print(minigame.winner, attack_sequence)
-                        # stop path when result
-                        G.nodes[n]['winner'] = minigame.winner
-                        G.nodes[n]['attack'] = attack
-                        continue
-                    newleavelist.append(n)
-            depth=depth+1
-        return G, root
-    
-    @staticmethod
-    def minimax(G, n):
-        """Perform minimax from node n on a NetworkX graph G.
-        Assume node n is a maximiser node.
-        Return node corresponding to best move
-        """
-
-        maxplayer = True
-        minplayer = False
-        def _minimax(G, n, player):
-
-            # Base case, winning node found
-            if G.nodes[n]["winner"]:
-                score = {'player1': 1, 'player2': -1}.get(G.nodes[n]['winner'])
-                G.nodes[n].update({'score': score})
-                return score
-
-            if player == maxplayer:
-                bestv = -1
-                for child in G.successors(n):
-                    v = _minimax(G, child, minplayer)
-                    G.nodes[child].update({'score': v})
-                    bestv = max(bestv, v)
-            else:
-                bestv = 1
-                for child in G.successors(n):
-                    v = _minimax(G, child, maxplayer)
-                    G.nodes[child].update({'score': v})
-                    bestv = min(bestv, v)
-            return bestv
-
-        # Find the best first move from the given node
-        # Assume given node n is a maximiser node.
-        best_move = None
-        bestv = -1
-
-        for child in G.successors(n):
-            v = _minimax(G, child, minplayer)
-            G.nodes[child].update({'score': v})
-
-            if v > bestv:
-                best_move = child
-                bestv = v
+    depth=1
+    # subsequent branches
+    while depth < max_depth:
+        leavelist = newleavelist[:]
+        newleavelist = []
+        for leave in leavelist:
             
-        if best_move:
-            return G.nodes[best_move]['attack']
-        else:
-            return None
+            # Get parent board
+            parent_board = Board(G.nodes[leave]['board'][0], G.nodes[leave]['board'][1])
+            for move in ALL_MOVES:
+                moves_available = parent_board.moves_available(player=depth%2)
+                if move not in moves_available:
+                    continue
+                
+                # Do move
+                new_board = update_board(Move(player=depth%2, move=move), parent_board)
+                
+                # Add move node to graph
+                n=n+1
+                G.add_node(n, move=move, 
+                           winner=new_board.is_winner(), 
+                           player=depth%2, board=new_board.state, 
+                           board_p=new_board.display())
+                G.add_edge(leave, n)
+                if new_board.is_winner():
+                    continue
+                    
+                newleavelist.append(n)
+        depth=depth+1
+    return G    
     
-    def move(self, game) -> str:
-        G, root = self.eval_game_tree(game)
-        best_move = self.minimax(G, root)
-        if best_move is None:
-            # make random move
-            return random.choice(game.get_possible_moves())
-        else:
-            return best_move
 
 
+def update_board(move:Move, board):
+    new_state = list(map(list, board.state))
+    # Apply move
+    new_state[move.non_player][move.move[1]] += board.state[move.player][move.move[0]]
+    # If any hand > 4, set to zero (out of the game)
+    if new_state[move.non_player][move.move[1]] > 4:
+        new_state[move.non_player][move.move[1]] = 0
 
+    new_board = Board(new_state[0], new_state[1])
+    return new_board
 
-class Game():
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.turn = 1
-        self.state = "in progress"
-        self.active = player1
-        self.inactive = player2
-        self.finished = False
-        self.winner = None
-        self.moves = []
-        self.graph = nx.DiGraph()
-        self.graph.add_node(0, attack=None, winner=None, player=None)
-        
-        
-        
-    def get_possible_moves(self):
-        # 'lr' - use active left hand to attack inactive right hand
-        moves = []
-        if self.active.left.count != 0:
-            if self.inactive.left.count != 0:
-                moves.append('ll')
-            if self.inactive.right.count != 0:
-                moves.append('lr')
-        if self.active.right.count != 0:
-            if self.inactive.left.count != 0:
-                moves.append('rl')
-            if self.inactive.right.count != 0:
-                moves.append('rr')
-        return moves
-    
-    
-    def switch_players(self):
-        self.active, self.inactive = self.inactive, self.active
-        
-    def zero_fingers(self):
-        # Go to zero if exceeds 4:
-        if self.inactive.left.count > 4:
-            self.inactive.left.count = 0
-        if self.inactive.right.count > 4:
-            self.inactive.right.count = 0
-            
-
-    def is_finished(self):
-        if self.player2.left.count == 0 and self.player2.right.count == 0:
-            self.finished = True
-            self.winner = self.player1.name
-            
-        if self.player1.left.count == 0 and self.player1.right.count == 0:
-            self.finished = True
-            self.winner = self.player2.name
-
-    def select_move(self):
-        possible_moves = self.get_possible_moves()
-        # random move
-        move = random.choice(possible_moves)        
-        return move
-        
-        
-    def do_move(self, move):
-        if move == 'll':
-            self.inactive.left.count += self.active.left.count
-        elif move == 'lr':
-            self.inactive.right.count += self.active.left.count
-        elif move == 'rr':
-            self.inactive.right.count += self.active.right.count
-        elif move == 'rl':
-            self.inactive.left.count += self.active.right.count
-        else:
-            # input not recognized
-            return
-        
-        # Add move to game history
-        self.moves.append(move)
-        # Zero fingers if move makes finger count > 4
-        self.zero_fingers()
-        # Check for winner
-        self.is_finished()
-        # Add node to graph
-        self.graph.add_node(self.turn, attack=move, winner=self.winner, player=self.active.name)
-        self.graph.add_edge(self.turn-1,self.turn)
-        # switch players
-        self.switch_players()
-        self.turn+=1
-        
-        return
-    
-    def check_moves(self, moves):
-        '''attacks: a list of attacks eg: ['ll', 'lr', ...]
-        returns True if moves are legal, false if not
-        '''
-        for move in moves:
-            if move not in self.get_possible_moves():
-                return False       
-            
-            self.do_move(move)
-        return True
-    
-    def draw(self, fig_size = (5,5), labels=None):
-        f, ax = plt.subplots(figsize=fig_size)
-        self.graph.graph.setdefault('graph', {})['rankdir'] = 'LR'
-        # color nodes based on winner
-        node_color = []
-        for node in self.graph.nodes(data=True):
-            if node[1]['winner'] == 'player1':
+def draw_graph(G, fig_size = (5,5), labels=None):
+    f, ax = plt.subplots(figsize=fig_size)
+    G.graph.setdefault('graph', {})['rankdir'] = 'LR'
+    # color nodes based on winner
+    node_color = []
+    for node in G.nodes(data=True):
+        if node[1]['winner']:
+            if node[1]['player'] == 0:
                 node_color.append('green')
-            elif node[1]['winner'] == 'player2':
+            if node[1]['player'] == 1:
                 node_color.append('red')
-            else:
-                node_color.append('lightgray')
-        pos = graphviz_layout(self.graph, prog='dot')
-        if labels:
-            ax = nx.draw_networkx(self.graph, pos=pos, labels=nx.get_node_attributes(G, labels), node_color=node_color)
         else:
-            ax = nx.draw_networkx(self.graph, pos=pos, node_color=node_color)
-        return ax
+            node_color.append('lightgray')
+    pos = graphviz_layout(G, prog='dot')
+    if labels:
+        ax = nx.draw_networkx(G, pos=pos, labels=nx.get_node_attributes(G, labels), node_color=node_color, node_size=20)
+    else:
+        ax = nx.draw_networkx(G, pos=pos, with_labels=False, node_color=node_color, node_size=20)
+    return ax
+
